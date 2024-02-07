038a0f56daf82bddc353702e2e84f2157959bf51
EDER <DANROMO1027@GMAIL.COM>
2024-02-06 23:01:59 -0500

    Udapted

--- interfaz.py
+++ interfaz.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 from tkinter import messagebox
 from time import strftime
 import pandas as pd
-#hola mundo
+#hola mundo 
 
 from Conexion import *
 
@@ -18,6 +18,7 @@ class FormularioAlmacen():
     global buscarCod
     buscarCod = None
     
+
     global nombre_buscado
     nombre_buscado = None
     
