d368c687ff21eed1cdfb3e072068c29a2e9fd8b1
EDER <DANROMO1027@GMAIL.COM>
2024-02-08 16:01:37 -0500

    proto

--- Almacen.py
+++ Almacen.py
@@ -1,42 +1,87 @@
 from Conexion import *
 
 class Alma:
+    
     def mostrar_productos():
+        
             try:
+                
                 cone = Conexion.ConexionBaseDatos()
                 if cone is None:
                     return []
                 cursor = cone.cursor()
-                cursor.execute("SELECT * FROM suministros.almacen;")
+                cursor.execute("SELECT * FROM suministros.usuarios;")
                 miResultado = cursor.fetchall()
                 cone.commit()
                 cone.close()
                 return miResultado
+            
             except mysql.connector.Error as error:
                 print("Error de muestra de datos {}".format(error))
-
-    def busca_producto(nombre_producto,nombre_material):
+                
+    def ingresarCantidad(canti):
         
         try:
+            
+            cone = Conexion.ConexionBaseDeDatos()
+            cursor = cone.cursor()
+            
+            if canti.get()=="Ingeniero 1":
+                sql = "insert into usuarios values(null,null,null,%s,%s,%s,%s,%s,null);"
+                valores = (canti,)
+            if canti.get()=="Ingeniero 2":
+                sql = "insert into usuarios values(null,null,null,%s,%s,%s,%s,%s,null);"
+                valores = (canti,)
+            if canti.get()=="Ingeniero 3":
+                sql = "insert into usuarios values(null,null,null,%s,%s,%s,%s,%s,null);"
+                valores = (canti,)
+
+            cursor.execute(sql,valores)
+            cone.commit()
+            print(cursor.rowcount, "registro ingresado")
+            
+        except mysql.connector.Error as error:
+            print("Error de ingreso de datos {}".format(error))
+    
+    def modificarClientes():
+        
+        try:
+            
+            cone = Conexion.ConexionBaseDeDatos()
+            cursor = cone.cursor()
+            sql = "UPDATE usuarios SET usuarios.Ing1 = %s, usuarios.Ing2 = %s, usuarios.Ing3 = %s, usuarios.Almacen = %s, usuarios.Total = %s where usuarios.id = %s;"
+           
+            valores = (nombres, apellidos, sexo, idUsuarios,)
+            
+            
+            cursor.execute(sql,valores)
+            
+            #Ejecucion de la funcion
+            cone.commit()
+            print(cursor.rowcount, "registro actualizado")
+        
+        except mysql.connector.Error as error:
+            print("Error al actualizar datos {}".format(error))
+
+
+    def busca_producto(nombre_producto):
+        
+        try:
+            
             cone = Conexion.ConexionBaseDatos()
             if cone is None:
                     return ["NO"]
             cursor = cone.cursor()
-            
-            sql = "SELECT * FROM almacen WHERE Codigo_Mat LIKE %s OR Material LIKE %s"
+            sql = "SELECT * FROM usuarios WHERE Codigo_Mat LIKE %s OR Material LIKE %s"
             val1=((f"%{nombre_producto}%", f"%{nombre_producto}%"))
-            val2=((f"%{nombre_material}%", f"%{nombre_material}%"))
             cursor.execute(sql,val1)
-            
-            #cursor2.execute(sql2, (f"%{nombre_material}%", f"%{nombre_material}%"))
             nombreX = cursor.fetchall()
-            #nombref = cursor.fetchall()
             cone.commit()
             cone.close()
             print(nombre_producto+"almacen sin codigo")
-            print(nombre_material+"almacen sin nombre")
             return nombreX 
+        
         except mysql.connector.Error as error:
             print("Error de busqueda de datos {}".format(error))
             return []     
-        
+    
--- interfaz.py
+++ interfaz.py
@@ -4,7 +4,6 @@ from tkinter import ttk
 from tkinter import messagebox
 from time import strftime
 import pandas as pd
-#pan
 
 from Conexion import *
 
@@ -14,48 +13,73 @@ class FormularioAlmacen():
     
     global frame1
     frame1 = None
-    
     global buscarCod
     buscarCod = None
-    
-
     global nombre_buscado
     nombre_buscado = None
-    
     global nombre_producto
     nombre_producto = None
-    
-    global nombre_material
-    nombre_material = None
-    
-    global buscarMat 
-    buscarMat = None
-    
+    global Cod1
+    Cod1 = None
+    global reserva
+    reserva = None
+    global sipro
+    sipro = None
+    global cantidad
+    cantidad = None
+
 def Formulario():
         
-    global frame1,tree,buscarCod,buscarMat,nombre_buscado,nombre_material
+    global frame1,tree,buscarCod,buscarMat,nombre_buscado,refrescar,Cod1,reserva,sipro,cantidad
         
     try:
         ventana = Tk()
         ventana.geometry("1200x800")
         ventana.title("REGISTRO DE ALMACEN")
             
-        frame1 = Frame(ventana,padx=10,pady=10)
-        frame1.pack(side=LEFT,anchor='nw',padx=10,pady=10,fill='both',expand=True)
-        frame1.config(width=20,height=40)
+        frame1 = LabelFrame(ventana,text="Inventario - Almacen",padx=10,pady=10)
+        frame1.pack(side="left", anchor='n',padx=10, pady=10, fill='both', expand=True)
+        frame1.config(width=5,height=5)
+        frame3 = LabelFrame(ventana,text="TABLA",padx=10,pady=10)
+        frame3.pack(side=RIGHT, anchor='n', fill='both', expand=True)
+        frame3.config(width=5, height=10)
         
+        #METODO BUSCAR
         buscarCod = StringVar()
-        buscarMat = StringVar()
-        
-        
         Label(frame1, text="Codigo De Material", width=15, font=("arial",12),padx=5,pady=5).grid(row=0,column=0)
-        Label(frame1, text="Descripcion Del Material",width=20, font=("arial",12),padx=5,pady=5).grid(row=1,column=0)
         Entry(frame1,width=15,textvariable=buscarCod).grid(row=0,column=1,padx=10,pady=10)
-        Entry(frame1,width=15,textvariable=buscarMat).grid(row=1,column=1,padx=10,pady=10)
-        Button(frame1, text="BUSCAR",width=15,command=buscar_nombre).grid(row=2,columnspan=2,padx=5,pady=5)
+        Button(frame1, text="BUSCAR",width=15,command=buscar_nombre).grid(row=1,columnspan=2,padx=5,pady=5)
         
-        tree = ttk.Treeview(frame1,columns=("CODIGO MATERIAL","MATERIAL - DESCRIPCION","GRUPO","INGENIERO 1",
-                                                    "INGENIERO 2","INGENIERO 3","ALMACEN","TOTAL","UNIDADES DE MEDIDA"),show='headings',height=20)
+        #METODO AGREGAR
+        LabelFrame(frame1,text= "Agregar materiales").grid(row=2,columnspan=2)
+        Label(frame1, text="Codigo De Material", width=15, font=("arial",12),padx=5,pady=5).grid(row=2,column=0)
+        Label(frame1, text="SIPRO", width=15, font=("arial",12),padx=5,pady=5).grid(row=3,column=0)
+        Label(frame1, text="RESERVA", width=15, font=("arial",12),padx=5,pady=5).grid(row=4,column=0)
+        Label(frame1, text="Cantidad a ingresar", width=15, font=("arial",12),padx=5,pady=5).grid(row=5,column=0)
+        Label(frame1, text="Asignar Material", width=15, font=("arial",12),padx=5,pady=5).grid(row=6,column=0)
+        
+        Cod1 = StringVar()
+        sipro = StringVar()
+        reserva = StringVar()
+        cantidad = StringVar()
+        
+        Entry(frame1,width=15,textvariable=Cod1).grid(row=2,column=1,padx=10,pady=10)
+        Entry(frame1,width=15,textvariable=sipro).grid(row=3,column=1,padx=10,pady=10)
+        Entry(frame1,width=15,textvariable=reserva).grid(row=4,column=1,padx=10,pady=10)
+        Entry(frame1,width=15,textvariable=cantidad).grid(row=5,column=1,padx=10,pady=10)
+        
+        
+        #Combobox para asignar material a un ing o almacen
+        AsignarMateriales = tk.StringVar()
+        Asigna = ttk.Combobox(frame1, values=["Ingeniero 1","Ingeniero 2", "Ingeniero 3", "Almacen"],textvariable=AsignarMateriales,width=28)
+        Asigna.grid(row=6,column=1,padx=5,pady=5)
+        AsignarMateriales.set("Asigne mat. a una persona")
+        
+        Button(frame1, text="Agregar",width=15).grid(row=7,columnspan=2)
+        
+        #TABLA DE DATOS
+        tree = ttk.Treeview(frame3,columns=("CODIGO MATERIAL","MATERIAL - DESCRIPCION","GRUPO","INGENIERO 1",
+                                                    "INGENIERO 2","INGENIERO 3","ALMACEN","TOTAL","UNIDADES DE MEDIDA"),show='headings',height=10)
         
         tree.column("# 1",anchor=CENTER, width=100)
         tree.column("# 2", width=270)
@@ -80,7 +104,10 @@ def Formulario():
         for row in Alma.mostrar_productos():
             tree.insert("","end",values=row)
         
-        tree.grid(row=0,rowspan=50,column=3, sticky=tk.NE)
+        tree.grid(row=1,rowspan=50,column=0, sticky=tk.NE)
+        
+        #Boton para refrescar la tabla de datos
+        Button(frame3, text="Refrescar",width=15,command=actualizarTreeView).grid(row=0,column=0)
         
         ventana.mainloop()
         
@@ -89,42 +116,31 @@ def Formulario():
 
 
 def buscar_nombre():
-        global nombre_producto, nombre_buscado,buscarCod,tree,dato,nombre_material
+        global nombre_producto, nombre_buscado,buscarCod,tree,dato,refrescar,Cod1,reserva,sipro,cantidad
         try:
-            
             if not buscarCod.get():
-                
                 nombre_producto = " "
                 nombre_producto = str(nombre_producto)
             else:  
                 nombre_producto = buscarCod.get()
                 nombre_producto = str(nombre_producto)
-            
-            if not buscarMat.get():
-    
-                nombre_material = " "
-                nombre_material = str(nombre_material)
-            else:
-    
-                nombre_material = buscarMat.get()
-                nombre_material = str(nombre_material)
-                
-            nombre_buscado = Alma.busca_producto(nombre_producto,nombre_material)
+            nombre_buscado = Alma.busca_producto(nombre_producto)
 
-            
             tree.delete(*tree.get_children())
             i = -1
+            
             for dato in nombre_buscado:
                 i= i+1                       
                 tree.insert('',i, text = nombre_buscado[i][1:1], values=nombre_buscado[i][0:9])
+                
             if nombre_buscado == []:
                 messagebox.showinfo("Informacion", "El codigo de material ingresado no es correcto")
+                
         except ValueError as error:
             print("Error al buscarCod, error: {}".format(error))
 
-
 def mostrar_todo():
-    global nombre_producto, nombre_buscado,buscarCod,tree
+    global nombre_producto, nombre_buscado,buscarCod,tree,refrescar,Cod1,reserva,sipro,cantidad,Asigna,asigna,reser,canti,codMat,sipro1
     
     tree.delete(*tree.get_children())
     registro = Alma.mostrar_productos()
@@ -133,6 +149,82 @@ def mostrar_todo():
         i= i+1                       
         tree.insert('',i, text = registro[i][1:2], values=registro[i][0:9])
 
-
+def actualizarTreeView():
+    global tree,datos,refrescar,Cod1,reserva,sipro,cantidad,asigna,reser,codMat,sipro1
+    
+    try:
+        tree.delete(*tree.get_children())
+        datos = Alma.mostrar_productos()
             
+        for row in Alma.mostrar_productos():
+                tree.insert("","end", values=row)
+    
+    except ValueError as error:
+        print("Error al actualizar tabla {}".format(error))
+
+def guardarRegistros():
+        
+        global tree,refrescar,Cod1,reserva,sipro,cantidad,Asigna,asigna,reser,codMat,sipro1,canti
+        
+        try:
+            if Cod1 is None or reserva is None or Asigna is None or cantidad is None or sipro is None:
+                print("Los widgets no estan inicializados")
+                return
+            
+            codMat = Cod1.get()
+            reser = reserva.get()
+            sipro1 = sipro.get()
+            canti = cantidad.get()
+            asigna = asigna.get()
+            
+                #Llamamos la clase y la funcion de clientes para asignar los valores de cada variable a la tabla
+            CClientes.ingresarClientes(nombres,apellidos,sexo)
+            
+             #Por medio del messagebox emitimos un cuadro de notificacion respecto al estado de los datos
+             #(titulo del cuadro de dialogo - texto informativo)
+            messagebox.showinfo("Informacion", "Los datos fueron guardados")
+            
+                #Llamamos la funcion para actualizar
+            actualizarTreeView()
+            
+                #Limpiamos los campos
+            textBoxNombres.delete(0,END)
+            textBoxApellidos.delete(0,END)
+        except ValueError as error:
+            print("Error al ingresar los datos {}".format(error))
+
+
+def modificarRegistros():
+        
+        global tree,refrescar,Cod1,reserva,sipro,cantidad,Asigna
+        
+        try:
+            
+                #verificar si los widgets (Editext) estan inicializados (contienen la informacion a registrar)
+            if textBoxId is None or textBoxNombres is None or textBoxApellidos is None or combo is None:
+                print("Los widgets no estan inicializados")
+                return
+            
+            idUsuario = textBoxId.get()
+            nombres = textBoxNombres.get()
+            apellidos = textBoxApellidos.get()
+            sexo = combo.get()
+            
+                #Llamamos la clase y la funcion de clientes para asignar los valores de cada variable a la tabla
+            CClientes.modificarClientes(idUsuario,nombres,apellidos,sexo)
+            
+                #Por medio del messagebox emitimos un cuadro de notificacion respecto al estado de los datos
+                #(titulo del cuadro de dialogo - texto informativo)
+            messagebox.showinfo("Informacion", "Los datos fueron actualizados")
+            
+            actualizarTreeView()
+            
+                #Limpiamos los campos
+            textBoxId.delete(0,END)
+            textBoxNombres.delete(0,END)
+            textBoxApellidos.delete(0,END)
+        except ValueError as error:
+            print("Error al ingresar los datos {}".format(error))
+
+      
 Formulario()
