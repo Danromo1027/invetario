e9a021a34c44f6662e0c14f2ccf9832942d48215
EDER <DANROMO1027@GMAIL.COM>
2024-02-08 19:56:23 -0500

    base de datos carga

--- Excel.py
+++ Excel.py
@@ -2,16 +2,12 @@ import pandas as pd
 
 from Conexion import *
 
-class Formato:
-    
-    def formatoExcel():
-        
-        cone = Conexion.ConexionBaseDatos()
-        query = "SELECT * FROM usuario;"
-        df = pd.read_sql(cone,query)
-        fri = pd.DataFrame(df)
-        fri.to_CSV('C:/Documentos/PROYECTOS DE PROGRAMACION/Forma.csv')
-        cone.commit()
-        cone.close()
+
+cone = Conexion.ConexionBaseDatos()
+query = "SELECT * FROM usuario;"
+df = pd.read_sql(cone,query)
+df.to_excel('C:/Users/diosc/OneDrive/Documentos/PROYECTOS_DE_PROGRAMACION/Forma.xlsx', index=False)
+cone.commit()
+cone.close()
         
     
--- usuarios.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM suministros.usuarios;
